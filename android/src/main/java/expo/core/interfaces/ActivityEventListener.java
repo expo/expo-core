8e0978e26ad3418b402f2bfad85730219bfbcc3c
Stanisław Chmiela <sjchmiela@gmail.com>
2018-11-06 13:06:45 +0100

    Deprecate repository in favor of expo/expo

--- android/src/main/java/expo/core/interfaces/ActivityEventListener.java
+++
@@ -1,8 +0,0 @@
-package expo.core.interfaces;
-
-import android.app.Activity;
-import android.content.Intent;
-
-public interface ActivityEventListener {
-  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data);
-}
